2159226a715b7711f38c9589443393de57ddd891
Zurab <mcgeni2003@mail.ru>
2024-01-23 14:31:36 +0300

    КаРтОшКи

---
+++ src/components/products/ProductCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styles from './card.module.css'
+
+const ProductCard = ({id, title, image, price, power, engineCapacity, accelerationTo100Km}) => {
+    console.log(image)
+    return (
+        <div className={styles.card}>
+            <div className={styles.image}>
+                <img src={`http://localhost:4100/image/${image[0]}`} alt="" />
+            </div>
+            <div>
+                {title}
+            </div>
+            <div className={styles.info}>
+                <p>Мощность:{power}</p>
+                <p>Разгон до 100 км/ч:{accelerationTo100Km}</p>
+                <p>Максимальная скорость:{engineCapacity}</p>
+            </div>
+            <div className={styles.price}>
+                <p>День:{price}p</p>
+            </div>
+            <div>
+                <button>Забронировать</button>
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard
